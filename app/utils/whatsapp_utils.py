ffe23da01b8c9d5de04adc0cbddd34dad3ca36ba
Aditya <2260361@cognizant.com>
2024-12-25 10:33:08 +0530

    added

---
+++ app/utils/whatsapp_utils.py
@@ -0,0 +1,77 @@
+import json
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+RECIPIENT_NUMBER = os.getenv("RECIPIENT_NUMBER")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+
+VERSION = os.getenv("VERSION")
+
+
+def generate_response(response):
+    # Return text in uppercase
+    return response.upper()
+
+
+def is_valid_whatsapp_message(body):
+    """
+    Check if the incoming webhook event has a valid WhatsApp message structure.
+    """
+    return (
+        body.get("object")
+        and body.get("entry")
+        and body["entry"][0].get("changes")
+        and body["entry"][0]["changes"][0].get("value")
+        and body["entry"][0]["changes"][0]["value"].get("messages")
+        and body["entry"][0]["changes"][0]["value"]["messages"][0]
+    )
+
+def get_text_message_input(recipient, text):
+    return json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": recipient,
+            "type": "text",
+            "text": {"preview_url": False, "body": text},
+        }
+    )
+
+def send_message(data):
+    headers = {
+        "Content-type": "application/json",
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+    }
+
+    url = f"https://graph.facebook.com/{VERSION}/{PHONE_NUMBER_ID}/messages"
+
+    response = requests.post(url, data=data, headers=headers, verify=False)
+    if response.status_code == 200:
+        print("Status:", response.status_code)
+        print("Content-type:", response.headers["content-type"])
+        print("Body:", response.text)
+        return response
+    else:
+        print(response.status_code)
+        print(response.text)
+        return response
+
+
+def process_whatsapp_message(body):
+    wa_id = body["entry"][0]["changes"][0]["value"]["contacts"][0]["wa_id"]
+    name = body["entry"][0]["changes"][0]["value"]["contacts"][0]["profile"]["name"]
+
+    message = body["entry"][0]["changes"][0]["value"]["messages"][0]
+    message_body = message["text"]["body"]
+
+
+    response = generate_response(message_body)
+
+    data = get_text_message_input(
+        recipient=RECIPIENT_NUMBER, text=response
+    )
+
+    send_message(data)
---
+++ main.py
@@ -0,0 +1,63 @@
+
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+
+from app.utils.whatsapp_utils import is_valid_whatsapp_message,process_whatsapp_message
+
+
+
+
+
+def verify(request: Request):
+    # Parse params from the webhook verification request
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+    verify_token = "hello"
+
+    # Check if a token and mode were sent
+    if mode and token:
+        # Check the mode and token sent are correct
+        if mode == "subscribe" and token == verify_token:
+            # Respond with 200 OK and challenge token from the request
+            print("WEBHOOK_VERIFIED")
+            return challenge
+        else:
+            # Responds with '403 Forbidden' if verify tokens do not match
+            print("VERIFICATION_FAILED")
+            raise HTTPException(status_code=403, detail="Verification failed")
+    else:
+        # Responds with '400 Bad Request' if verify tokens do not match
+        print("MISSING_PARAMETER")
+        raise HTTPException(status_code=400, detail="Missing parameters")
+
+
+
+
+app = FastAPI()
+
+
+
+@app.get("/")
+def home():
+    return "WhatsApp OpenAI Webhook is listening!"
+
+
+@app.get("/webhook")
+def webhook(request: Request):
+    return verify(request)
+
+@app.post("/webhook")
+def handle_message(request: Request):
+    body = request.json()
+    if is_valid_whatsapp_message(body):
+        process_whatsapp_message(body)
+        return JSONResponse(content={"status": "ok"}, status_code=200)
+    return JSONResponse(content={"status": "invalid message"}, status_code=400)
+
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+
